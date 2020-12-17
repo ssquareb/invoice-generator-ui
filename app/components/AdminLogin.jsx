a398319827741adb55c0ee013ed94cd750fc9953
srik007 <srikanth@everest.engineering>
2020-12-17 21:19:30 +0530

    Refacot the url config file

--- app/components/AdminLogin.jsx
+++ app/components/AdminLogin.jsx
@@ -1,69 +1,95 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
-import { Container, Row, Form, FormGroup, FormControl, FormLabel, Button, Alert, Table } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Form,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Button,
+  Alert,
+  Table,
+} from 'react-bootstrap';
 import '../../docs/css/login.css';
+import {API_URL} from '../config/app.config';
 class AdminLogin extends React.Component {
-    constructor(props) {
-    super(props);
-    this.state={email:'',password:''};
-    this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(event){
-        let name=event.target.name;
-        let value=event.target.value;
-        this.setState({
-			[name]: value
-		});
-    }
-    handleSubmit = ()=>{
-        if(this.state.email==''||this.state.password=='')
-        {
-            document.getElementById('login').innerHTML='Please enter valid inputs'
-        }
-        else {
-        fetch("http://localhost:9000/v1/auth/login",{
-            method:"post",
-            
-            headers: {
-                'Content-Type':'application/json'
-                
-            },
-            body: JSON.stringify({ email: this.state.email,password: this.state.password})
+  constructor (props) {
+    super (props);
+    this.state = {email: '', password: ''};
+    this.handleChange = this.handleChange.bind (this);
+  }
+  handleChange (event) {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState ({
+      [name]: value,
+    });
+  }
+  handleSubmit = () => {
+    if (this.state.email == '' || this.state.password == '') {
+      document.getElementById ('login').innerHTML = 'Please enter valid inputs';
+    } else {
+      fetch (`${API_URL}v1/auth/login`, {
+        method: 'post',
+
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify ({
+          email: this.state.email,
+          password: this.state.password,
+        }),
+      })
+        .then (res => res.json ())
+        .then (res => {
+          let token = res.tokens.access.token;
+          localStorage.setItem ('token', 'Bearer ' + token);
+          this.props.history.push ('/users');
         })
-        .then(res => res.json()).then(res => {
-            let token = res.tokens.access.token;
-            localStorage.setItem("token", "Bearer "+token);
-            this.props.history.push('/users'); 
-        }    
-        )
-    .catch((err)=>{
-        console.log(err);
-        
-    })
+        .catch (err => {
+          console.log (err);
+        });
+    }
+  };
+
+  render () {
+    return (
+      <div className="form">
+        <h1>Admin Login</h1>
+        <div>
+          <FormGroup>
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={this.handleChange}
+              value={this.state.email}
+              className="input"
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              placeholder="Password"
+              className="input"
+            />
+          </FormGroup>
+          <p id="login" />
+          <Button onClick={() => this.handleSubmit ()}>SignIn</Button>
+          <Button onClick={() => this.props.history.push ('/register')}>
+            SignUp
+          </Button>
+        </div>
+
+      </div>
+    );
   }
 }
-    
-    render() {
-    return (
-    <div className='form'>
-       <h1>Admin Login</h1>
-        <div >
-        <FormGroup>
-			   <FormLabel>Email</FormLabel>
-				<FormControl type="email" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email} className='input'></FormControl>
-			</FormGroup>                
-			<FormGroup>
-				<FormLabel>Password</FormLabel>
-				<FormControl type="password" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" className='input'></FormControl>
-			</FormGroup>  
-            <p id='login'></p>
-            <Button onClick={()=> this.handleSubmit()}>SignIn</Button>            
-            <Button onClick={()=> this.props.history.push('/register')}>SignUp</Button>  
-        </div>   
-                            
-    </div>
-    )
-    }
-    }
 
-export default withRouter(AdminLogin);
+export default withRouter (AdminLogin);
--- app/components/users.jsx
+++ app/components/users.jsx
@@ -1,152 +1,192 @@
-import React from "react";
-import {withRouter} from 'react-router-dom'
+import React from 'react';
+import {withRouter} from 'react-router-dom';
 import LogOut from 'components/logout';
-import { Container, Row, Form, FormGroup, FormControl, FormLabel, Button, Alert, Table } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Form,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Button,
+  Alert,
+  Table,
+} from 'react-bootstrap';
 import '../../docs/css/usersTable.css';
+import {API_URL} from '../config/app.config';
+
 class Users extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-            userId:'',
-            name:'',
-            planName:'',
-            planStart:'',
-            planEnd:'',
-            address:'',
-            location: "",
-            mobile:'',
-			users: [],
-			showAlert: false,
-			alertMsg: "",
-			alertType: "success",
-			userId: "",
-			update: false,
-		};
-	}
+  constructor (props) {
+    super (props);
+    this.state = {
+      userId: '',
+      name: '',
+      planName: '',
+      planStart: '',
+      planEnd: '',
+      address: '',
+      location: '',
+      mobile: '',
+      users: [],
+      showAlert: false,
+      alertMsg: '',
+      alertType: 'success',
+      userId: '',
+      update: false,
+    };
+  }
 
-	handleChange = (evt) => {
-		this.setState({
-			[evt.target.name]: evt.target.value,
-		});
-	};
+  handleChange = evt => {
+    this.setState ({
+      [evt.target.name]: evt.target.value,
+    });
+  };
 
-	componentDidMount() {
-		var token = localStorage.getItem("token");
-		if(token) {
-			this.fetchAllUsers();
-		}
-		else {
-			this.props.history.push("/login");
-		}
-	}
+  componentDidMount () {
+    var token = localStorage.getItem ('token');
+    if (token) {
+      this.fetchAllUsers ();
+    } else {
+      this.props.history.push ('/login');
+    }
+  }
 
-	fetchAllUsers = () => {
-		
-		fetch("http://localhost:9000/v1/users", {
-			method: "GET",
-			headers: {Authorization:localStorage.getItem("token")},
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log("result", data);
-				this.setState({
-					users: data.results,
-				});
-			})
-			.catch((error) => console.log("error", error));
-	};
+  fetchAllUsers = () => {
+    fetch (`${API_URL}v1/users`, {
+      method: 'GET',
+      headers: {Authorization: localStorage.getItem ('token')},
+    })
+      .then (response => response.json ())
+      .then (data => {
+        console.log ('result', data);
+        this.setState ({
+          users: data.results,
+        });
+      })
+      .catch (error => console.log ('error', error));
+  };
 
-	deleteUser = (userId) => {
-		
-		fetch(`http://localhost:9000/v1/users/${userId}`, {
-			method: "DELETE",
-			headers: {Authorization:localStorage.getItem("token")},
-		})
-			.then((response) => response.json())
-			.then((result) => {
-				this.setState({
-					showAlert: true,
-					alertMsg: result.response,
-					alertType: "danger",
-				});
-				this.fetchAllUsers();
-			})
-			.catch((error) => console.log("error", error));
-	};
+  deleteUser = userId => {
+    fetch (`${API_URL}v1/users/${userId}`, {
+      method: 'DELETE',
+      headers: {Authorization: localStorage.getItem ('token')},
+    })
+      .then (response => response.json ())
+      .then (result => {
+        this.setState ({
+          showAlert: true,
+          alertMsg: result.response,
+          alertType: 'danger',
+        });
+        this.fetchAllUsers ();
+      })
+      .catch (error => console.log ('error', error));
+  };
 
+  render () {
+    return (
+      <div>
+        <Button
+          onClick={() =>
+            this.props.history.push ('/users/create', {
+              update: false,
+              user: {},
+            })}
+          className="button"
+        >
+          AddUser
+        </Button>
 
-	render() {
-		return (
-			<div>
-				<Button onClick={()=> this.props.history.push('/users/create',{update: false, user:{}})} className='button'>AddUser</Button>
-				
-				<Container>
-					{this.state.showAlert === true ? (
-						<Alert
-							variant={this.state.alertType}
-							onClose={() => {
-								this.setState({
-									showAlert: false,
-								});
-							}}
-							dismissible
-						>
-							<Alert.Heading>{this.state.alertMsg}</Alert.Heading>
-						</Alert>
-					) : null}
-					<Row>
-						<Table striped bordered hover size="sm" id='users'>
-							<thead>
-								<tr>
-									<th>UserId</th>
-									<th>Name</th>
-                                    <th>PlanName</th>
-                                    <th>PlanStart</th>
-                                    <th>PlanEnd</th>
-                                    <th>Address</th>
-                                    <th>Location</th>
-                                    <th>Mobile</th>
-									<th colSpan="2">Actions</th>
-								</tr>
-							</thead>
-							<tbody>
-								{this.state.users && this.state.users.map((user) => {
-									var planStartDate = new Date(user.planStart);
-									var planEndDate = new Date(user.planEnd);
-									var planStart=planStartDate.getFullYear()+'/' + (planStartDate.getMonth()+1) + '/'+planStartDate.getDate();
-									var planEnd=planEndDate.getFullYear()+'/' + (planEndDate.getMonth()+1) + '/'+planEndDate.getDate();
-									return (
-										<tr>
-											<td>{user.userId}</td>
-											<td>{user.name}</td>
-                                            <td>{user.planName}</td>
-                                            <td>{planStart}</td>
-                                            <td>{planEnd}</td>
-                                            <td>{user.address}</td>
-											<td>{user.location}</td>
-                                            <td>{user.mobile}</td>
-											<td>
-											<Button variant="info" onClick={()=> this.props.history.push('/users/create',{update: true, user})} className='button'>
-													Edit
-												</Button>
-											</td>
-											<td>
-												<Button variant="danger" onClick={() => this.deleteUser(user.userId)} className='delete'>
-													Delete
-												</Button>
-											</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</Table>
-					</Row>
-				</Container>
-				<LogOut/> 
+        <Container>
+          {this.state.showAlert === true
+            ? <Alert
+                variant={this.state.alertType}
+                onClose={() => {
+                  this.setState ({
+                    showAlert: false,
+                  });
+                }}
+                dismissible
+              >
+                <Alert.Heading>{this.state.alertMsg}</Alert.Heading>
+              </Alert>
+            : null}
+          <Row>
+            <Table striped bordered hover size="sm" id="users">
+              <thead>
+                <tr>
+                  <th>UserId</th>
+                  <th>Name</th>
+                  <th>PlanName</th>
+                  <th>PlanStart</th>
+                  <th>PlanEnd</th>
+                  <th>Address</th>
+                  <th>Location</th>
+                  <th>Mobile</th>
+                  <th colSpan="2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.users &&
+                  this.state.users.map (user => {
+                    var planStartDate = new Date (user.planStart);
+                    var planEndDate = new Date (user.planEnd);
+                    var planStart =
+                      planStartDate.getFullYear () +
+                      '/' +
+                      (planStartDate.getMonth () + 1) +
+                      '/' +
+                      planStartDate.getDate ();
+                    var planEnd =
+                      planEndDate.getFullYear () +
+                      '/' +
+                      (planEndDate.getMonth () + 1) +
+                      '/' +
+                      planEndDate.getDate ();
+                    return (
+                      <tr>
+                        <td>{user.userId}</td>
+                        <td>{user.name}</td>
+                        <td>{user.planName}</td>
+                        <td>{planStart}</td>
+                        <td>{planEnd}</td>
+                        <td>{user.address}</td>
+                        <td>{user.location}</td>
+                        <td>{user.mobile}</td>
+                        <td>
+                          <Button
+                            variant="info"
+                            onClick={() =>
+                              this.props.history.push ('/users/create', {
+                                update: true,
+                                user,
+                              })}
+                            className="button"
+                          >
+                            Edit
+                          </Button>
+                        </td>
+                        <td>
+                          <Button
+                            variant="danger"
+                            onClick={() => this.deleteUser (user.userId)}
+                            className="delete"
+                          >
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+          </Row>
+        </Container>
+        <LogOut />
 
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 }
 
-export default withRouter(Users);
+export default withRouter (Users);
